ac1d99be9e6873c1bd87d9cb032eb570926bbbbd
mosraful21 <mosraful21@gmail.com>
2022-11-26 19:36:42 +0600

    added user data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port =  process.env.PORT || 5000;
 
 const app = express();
@@ -8,16 +10,46 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// load products
-const products = require('./Products.json');
-app.get('/products', (req, res) => {
-    res.send(products)
-})
-app.get('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const seletedProduct= products.find(n => n.id === id)
-    res.send(seletedProduct);
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntn7mgs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const productMarketingCollection = client.db('productMarketing').collection('products');
+        const usersCollection = client.db('productMarketing').collection('users');
+
+        // Product data load ----------------------------------
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const options = await productMarketingCollection.find(query).toArray();
+            res.send(options);
+        });
+
+        app.get('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productMarketingCollection.findOne(query);
+            res.send(result);
+        });
+
+        // Users Data load --------------------------------------------
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+        
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
+
+
+
 
 app.get('/', async (req, res) => {
     res.send('Product marketing server running on server')
