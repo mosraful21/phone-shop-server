9155bc99c38a01f43d62951fb76ec0fcfbd3c8cc
mosraful21 <mosraful21@gmail.com>
2022-11-28 04:04:20 +0600

    booking added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     const productMarketingCollection = client.db('productMarketing').collection('products');
     const usersCollection = client.db('productMarketing').collection('users');
     const bookingsCollection = client.db('productMarketing').collection('bookings');
+    const wishListCollection = client.db('productMarketing').collection('wishlist');
 
     // Product data load ----------------------------------
     app.get('/products', async (req, res) => {
@@ -46,6 +47,77 @@ async function run() {
       const users = await usersCollection.find(query).toArray();
       res.send(users);
     });
+
+
+    // bookings from users
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      const query = {
+        BuyerName: booking.name,
+        email: booking.email,
+        phone: booking.phone,
+        productName: booking.productName,
+        resalePrice: booking.resalePrice,
+        location: booking.location,
+      }
+      const Booked = await bookingsCollection.find(query).toArray();
+      if (Booked.length) {
+        const message = `You Already buy this ${booking.productName}`
+        return res.send({ acknowledge: false, message });
+      }
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result)
+    });
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+
+      // const decodeEmail = req.decoded.email;
+      // if (email !== decodeEmail) {
+      //   return res.status(403).send({ message: 'forbidden access' });
+      // }
+
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
+    //   wish list
+    app.post('/wishlist', async (req, res) => {
+      const wishlist = req.body;
+      const query = {
+        name: wishlist.name,
+        email: wishlist.email,
+        productName: wishlist.productName,
+        resalePrice: wishlist.resalePrice,
+        originalPrice: wishlist.originalPrice
+      }
+      const result = await wishListCollection.insertOne(query);
+      res.send(result)
+    });
+    app.get('/wishlist', async (req, res) => {
+      const email = req.query.email;
+      // const decodeEmail = req.decoded.email;
+      // if (email !== decodeEmail) {
+      //   return res.status(403).send({ message: 'forbidden access' });
+      // }
+
+      const query = { email: email };
+      const wishlist = await wishListCollection.find(query).toArray();
+      res.send(wishlist);
+    })
+
+    //   jwt
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+        return res.send({ accessToken: token })
+      }
+      res.status(403).send({ accessToken: '' })
+
+    })
 
   }
   finally {
